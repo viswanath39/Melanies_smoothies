02c99a332905b94910acf8cf73a91794177a322a
Viswanath09 <49748747+viswanath39@users.noreply.github.com>
2025-12-18 21:39:24 +0530

    Removed get_active_session in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write("The name on smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
