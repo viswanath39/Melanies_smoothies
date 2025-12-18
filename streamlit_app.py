9582996e5b5fd3ce0bbfca13ddb20af7642f2e8d
Viswanath09 <49748747+viswanath39@users.noreply.github.com>
2025-12-18 22:21:09 +0530

    Integrate fruit nutrition API into smoothie app
    Added functionality to fetch and display nutrition information for selected fruits using an API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -31,6 +32,9 @@ if ingredients_list:
      ingredients_string =''
      for each_fruit in ingredients_list:
           ingredients_string += each_fruit
+          st.subheader(each_fruit + 'Nutrition Information')
+          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + each_fruit)
+          sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
          
      st.write(ingredients_string)
 
@@ -46,6 +50,5 @@ if ingredients_list:
          
          st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
+
